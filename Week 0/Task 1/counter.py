82545a93bdcd87d5022a2be4ae0ae0ee28a93179
NihonSiam <nihonsiam.personal@gmail.com>
2026-02-03 22:36:24 +0200

    #

--- Week 0/Task 1/counter.py
+++ Week 0/Task 1/counter.py
@@ -1,12 +1,12 @@
 class Counter:
     def __init__(self):
-        self.__count = 0  # count starts at 0
+        self.__count = 0  # it starts from 0
 
     def addCount(self):
-        self.__count += 1  # increase count by 1
+        self.__count += 1  # add 1 to count
 
     def getCount(self):
         return self.__count  # return current count
 
     def zeroCount(self):
-        self.__count = 0  # set count back to 0
+        self.__count = 0  # remove all count and set to 0
